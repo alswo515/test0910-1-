9599b4aa0c963233945640fe8ea398b583f13785
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-10 16:22:07 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -13,22 +13,15 @@
     )
 
 
+
     $('#kimBox').load('main.html')
 
-    $('.topmenu>a').on('click', function (e) {
+    $('.topmenu > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#kimContainer').remove()
         $('#kimBox').load(url)
     })
-    $('.h1Nav > h1').on('click', function (e) {
-        e.preventDefault()
-        $('#newContainer').remove()
-        $('#newBox').load('main.html')
-    })
-
-
-
 
 
 
